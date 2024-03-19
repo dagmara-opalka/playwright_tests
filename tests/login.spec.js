7f638aad989be433f76607698fe56dc7a1b8b0df
dagmara-opalka <dagmara-opalka@wp.pl>
2024-03-19 20:20:47 +0100

    lesson 3

---
+++ tests/login.spec.js
@@ -0,0 +1,23 @@
+import { expect, test } from "@playwright/test";
+
+test.describe('User login to Demobank', () => {
+  test("login with correct credentials", async ({ page }) => {
+    await page.goto("https://demo-bank.vercel.app/");
+    await page.getByTestId("login-input").click();
+    await page.getByTestId("login-input").fill("testerLO");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("haslo123");
+    await page.getByTestId("login-button").click();
+    await expect(page.getByTestId("user-name")).toBeVisible();
+  });
+
+  test("unsuccessful login with too short username", async ({ page }) => {
+    await page.goto("https://demo-bank.vercel.app/");
+    await page.getByTestId("login-input").click();
+    await page.getByTestId("login-input").fill("tester");
+    await page.getByTestId("password-input").click();
+    await expect(page.getByTestId("error-login-id")).toHaveText(
+      "identyfikator ma min. 8 znaków"
+    );
+  });
+});
