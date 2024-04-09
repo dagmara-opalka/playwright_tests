b1a1f4b61fc479f8921d47195d95203ba77882ec
dagmara-opalka <dagmara-opalka@wp.pl>
2024-04-09 21:16:56 +0200

    frist aaa implementation

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -1,27 +1,41 @@
 import { expect, test } from "@playwright/test";
 
+
+
 test.describe("Dashboard test", () => {
   test("quick payment with correct data", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("testerLO");
-    await page.getByTestId("password-input").fill("12345678");
+    //Arrange
+    const url = "https://demo-bank.vercel.app/";
+    const userID = "testerLO";
+    const userPassword = "12345678";
+
+    const reciverID = "2";
+    const transferAmount = "120";
+    const transferTitle = "Zwrot";
+    const expectedTransferReceiver = 'Chuck Demobankowy'
+    //Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userID);
+    await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
-    await page.locator("#widget_1_transfer_receiver").selectOption("2");
+    await page.locator("#widget_1_transfer_receiver").selectOption(reciverID);
     await page.locator("#widget_1_transfer_amount").click();
-    await page.locator("#widget_1_transfer_amount").fill("120");
+    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
     await page.locator("#widget_1_transfer_title").click();
     await page.locator("#widget_1_transfer_title").press("CapsLock");
     await page.locator("#widget_1_transfer_title").fill("Z");
     await page.locator("#widget_1_transfer_title").press("CapsLock");
-    await page.locator("#widget_1_transfer_title").fill("Zwrot");
+    await page.locator("#widget_1_transfer_title").fill(transferTitle);
     await page.getByRole("button", { name: "wykonaj" }).click();
     await page.getByTestId("close-button").click();
     //await page.getByRole('link', { name: 'Przelew wykonany! Chuck' }).click();
+    
+    //Assert
     await expect(page.locator("#show_messages")).toHaveText(
-      "Przelew wykonany! Chuck Demobankowy - 120,00PLN - Zwrot"
+      `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
     );
   });
-  test.only("sucessful mobile top-up Dagmara", async ({ page }) => {
+  test("sucessful mobile top-up Dagmara", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
     await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("testerDa");
@@ -36,6 +50,8 @@ test.describe("Dashboard test", () => {
     await page.getByLabel("Doładowanie wykonane").dblclick();
     await page.getByText("Doładowanie wykonane", { exact: true }).click();
     // await expect(page.getByTestId("ui-id-1")).toHaveText("Doładowanie wykonane");
-    await expect(page.locator("#show_messages")).toHaveText("Doładowanie wykonane! 140,00PLN na numer 502 xxx xxx");
+    await expect(page.locator("#show_messages")).toHaveText(
+      "Doładowanie wykonane! 140,00PLN na numer 502 xxx xxx"
+    );
   });
 });
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,21 @@
 import { expect, test } from "@playwright/test";
 
+
 test.describe('User login to Demobank', () => {
   test("login with correct credentials", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("testerLO");
-    await page.getByTestId("password-input").fill("haslo123");
-    await page.getByTestId("login-button").click();
-    await expect(page.getByTestId("user-name")).toBeVisible();
+
+    //Arrange
+const url = "https://demo-bank.vercel.app/";
+const userPassword = "haslo123";
+const userID = "testerLO";
+const expectedUserName = "user-name";
+
+  //Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userID);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click(userPassword);
+    await expect(page.getByTestId(expectedUserName)).toBeVisible();
   });
 
   test("unsuccessful login with too short username", async ({ page }) => {
